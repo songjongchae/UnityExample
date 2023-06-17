a6a63afb325a5d1a70cb36f1e612f88009e2eceb
Administrator <aaaa1ass@gmail.com>
2023-06-17 16:40:09 +0900

    (2023.6.17) 광원

---
+++ It's mine/Assets/Light/Script/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Vector3 direction = Vector3.forward;
+
+    void Update()
+    {
+        tramsform.Translate(direction * Time.deltaTime);
+
+        if (transform.position.x >= -2.5f)
+        {
+            direction = Vector3.forward;
+            transform.localScale = new Vector3(3.5f, 3.5f, 3.5f);
+        }
+      
+        else if(transform.position.x <= -2.5f)
+        {
+            direction = Vector3.back;
+            transform.localScale = new Vector3(3.5f, 3.5f, -3.5f);
+        }
+    }
+}
